2c961ea9d37e5cc7d375a1c730a10252580309e2
Arr <anggarizki123@outlook.com>
2024-02-21 14:53:06 +0700

    first commit

--- public/js/script.js
+++ public/js/script.js
@@ -10,34 +10,34 @@ document.getElementById('Button').addEventListener('click', function() {
          but.disabled = false;
         }, 6000);
     }
-});// disabled input button 6 detik
+});
 
-const lebih = document.getElementById('rupiahInput');// 200 max
-var inputs = document.getElementById('rupiahInput'); // hapus isi input
-var textarea = document.getElementById('resultTextarea');// hapus isi textarea
-var click = 0;// hapus isi textarea
-var clicks = 0;// hapus isi input
+const lebih = document.getElementById('rupiahInput');
+var inputs = document.getElementById('rupiahInput'); 
+var textarea = document.getElementById('resultTextarea');
+var click = 0;
+var clicks = 0;
 inputs.addEventListener('click', function() {
     clicks++;
     if (clicks === 4) {
         inputs.value = '';
         clicks = 0;
     }
-}), // hapus isi input
+}), 
 textarea.addEventListener('click', function() {
     click++;
     if (click === 4) {
         textarea.value = '';
         click = 0;
     }
-}),// hapus isi textarea
+}),
 lebih.addEventListener('input', function() {
     if (this.value.length > 200) {
         alert('Input should be maximum 200 characters.');
         this.value = this.value.substring(0, 200);
         lebih.value = '';
     }
-});// 200 max
+});
 
 function cegah() {
     var input = document.getElementById("rupiahInput");
@@ -45,34 +45,34 @@ function cegah() {
     if (!regex.test(input.value)) {
         alert("Please enter only numbers, If you delete click 3x/4x on Number");
         input.value = "";
-    }// jika bukan berisi angka salah
+    }
 
     var inputnilai = document.getElementById('rupiahInput').value;
     var jalaninput = ambil(inputnilai);
-    document.getElementById('rupiahInput').value = jalaninput;// jika berisi XSS maka %
+    document.getElementById('rupiahInput').value = jalaninput;
 }
 function ambil(input) {
     return input.replace(/[<>&"']/g, '%');
-}// jika berisi XSS maka %
+}
 
 function convertCurrency() {
     
-    var inputValue = document.getElementById('rupiahInput').value;// tidak di jalankan jika input kosong
+    var inputValue = document.getElementById('rupiahInput').value;
     var buttonputar = document.getElementById('Button');
     var svgputar = document.getElementById('mySvg');
     if (inputValue.trim() === '') {
         return;
-    }// tidak di jalankan jika input kosong
+    }
     svgputar.style.animation = 'putar 1s linear infinite';
     setTimeout(function() {
         svgputar.style.animation = 'none';
-    }, 6000);// berputar 6 detik ketika button click
+    }, 6000);
     buttonputar.style.animation = 'bulat 1s linear infinite';
     setTimeout(function() {
         buttonputar.style.animation = 'none';
-    }, 6000);// berputar 6 detik ketika button click BUTTON PUTAR
+    }, 6000);
 
-    const rupiah = document.getElementById('rupiahInput').value;// kirim express js 
+    const rupiah = document.getElementById('rupiahInput').value;
     fetch('/convert', {
      method: 'POST',
         headers: {
